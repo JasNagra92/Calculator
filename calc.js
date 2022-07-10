a824e8554fd3c3e09482e6c9e91370fab0704c8c
newbiejas <jaskarannagra@homail.com>
2022-07-09 20:01:41 -0700

    add variables to store user input before and after operator is pressed, along with seperate functions to call for each operator

---
+++ calc.js
@@ -0,0 +1,10 @@
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let total = "";
+
+const sum = (a , b) => a + b;
+const subtract = (a , b) => a - b;
+const multiply = (a , b) => a * b;
+const divide = (a , b) => a / b;
+ 
