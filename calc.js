cdecf300d5d787b28a1239d0403ae30fa466d453
newbiejas <jaskarannagra@homail.com>
2022-07-09 20:14:48 -0700

    Dynamically create 10 buttons to use as numberpad

--- calc.js
+++ calc.js
@@ -7,4 +7,10 @@ const sum = (a , b) => a + b;
 const subtract = (a , b) => a - b;
 const multiply = (a , b) => a * b;
 const divide = (a , b) => a / b;
- 
+let btn = "";
+
+for (i = 0; i < 10; i ++){
+let btn = document.createElement('button')
+let numPad = document.getElementById("numberpad");
+numPad.appendChild(btn)
+}
